30e2501e042a1554c5467ece96ecba5491df5649
PJryn <peregrinryan@gmail.com>
2022-10-25 20:44:48 +1100

    Adjusted names of tables within queries.sql and values

--- queries.sql
+++ queries.sql
@@ -1,6 +1,6 @@
-CREATE TABLE "imdb" (
+CREATE TABLE "imdb_top_1000" (
     "film_name" VARCHAR(1050),
-    "released_year" Date,
+    "released_year" INT,
     "runtime" INT,
     "genre" VARCHAR(50),
     "imdb_rating" INT,
@@ -9,19 +9,19 @@ CREATE TABLE "imdb" (
     "actors" VARCHAR(100),
     "votes" BIGINT,
     "gross" money,
-    CONSTRAINT "pk_imdb" PRIMARY KEY (
+    CONSTRAINT "pk_imdb_top_1000" PRIMARY KEY (
         "film_name"
      )
 );
 
-CREATE TABLE "academy_award_winners" (
+CREATE TABLE "academy_award_data" (
     "film_name" VARCHAR(1050),
-    "ceremony_year" Date,
+    "ceremony_year" INT,
     "award" VARCHAR(50),
     "winner" boolean,
     "winner_name" VARCHAR(50)
 );
 
-ALTER TABLE "academy_award_winners" ADD CONSTRAINT "fk_academy_award_winners_film_name" FOREIGN KEY("film_name")
-REFERENCES "imdb" ("film_name");
+ALTER TABLE "academy_award_data" ADD CONSTRAINT "fk_academy_award_data_film_name" FOREIGN KEY("film_name")
+REFERENCES "imdb_top_1000" ("film_name");
 
